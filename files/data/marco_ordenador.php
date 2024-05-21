d4cfe7c84dec52cd5bd275eea18ddbbb1cde129d
Jaime Cruz <jaimecruz.dev@gmail.com>
2024-05-21 11:15:24 -0500

    peform: Nuevo formato de importación de datos

--- files/data/marco_ordenador.php
+++ files/data/marco_ordenador.php
@@ -1,9 +1,12 @@
 <?php
 
 return [
-    ['sigla' => 'SINIA', 'nombre' => 'Temáticas del Sistema Nacional de Información Ambiental'],
-    ['sigla' => 'MDEA',  'nombre' => 'Marco para el Desarrollo de las Estadísticas Ambientales'],
-    ['sigla' => 'ODS',   'nombre' => 'Objetivos de Desarrollo Sostenible'],
-    ['sigla' => 'OCDE',  'nombre' => 'Organización para la Cooperación y el Desarrollo Económico'],
-    ['sigla' => 'PNA',   'nombre' => 'Política Nacional del Ambiente'],
+    'fields' => ['sigla', 'nombre'],
+    'data' => [
+        ['SINIA', 'Temáticas del Sistema Nacional de Información Ambiental'],
+        ['MDEA',  'Marco para el Desarrollo de las Estadísticas Ambientales'],
+        ['ODS',   'Objetivos de Desarrollo Sostenible'],
+        ['OCDE',  'Organización para la Cooperación y el Desarrollo Económico'],
+        ['PNA',   'Política Nacional del Ambiente'],
+    ],
 ];
